63f01bba572a5efc7d55d26d4c10291ed2c543ea
steve1994 <steve.harnadi@gmail.com>
2015-12-02 11:39:13 +0700

    Add write new inverted file in experiment to debug

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("interface1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
 
